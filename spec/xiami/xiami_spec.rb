28b718e1732de54ac77c100e2fd16de88f686e3e
Forrest Ye <afu@forresty.com>
2015-01-05 18:15:33 +0800

    use hacky way to avoid parsing html

--- spec/xiami/xiami_spec.rb
+++ spec/xiami/xiami_spec.rb
@@ -7,10 +7,6 @@ describe Xiami do
     it { should respond_to :fetch_large_album_art= }
 
     describe '.fetch_large_album_art' do
-      it 'by default does not fetch large album art' do
-        Xiami.fetch_large_album_art.should == false
-      end
-
       it 'sets and gets' do
         Xiami.fetch_large_album_art = true
         Xiami.fetch_large_album_art.should == true
