bfe49d88879df177d3dcacc9d4e5b0afd64e065c
Forrest Ye <afu@forresty.com>
2015-01-12 19:24:12 +0800

    album fixture

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,6 +15,10 @@ end
 
 include WebMock::API
 
-pattern = /http:\/\/www\.xiami\.com\/widget\/xml-single\/uid\/0\/sid\/(\d+)/
+song_pattern = /http:\/\/www\.xiami\.com\/widget\/xml-single\/uid\/0\/sid\/(\d+)/
 
-stub_request(:any, pattern).to_return(lambda { |request| { body: fixture('songs/' + request.uri.path.match(/(\d+)$/)[0]) } })
+stub_request(:any, song_pattern).to_return(lambda { |request| { body: fixture('songs/' + request.uri.path.match(/(\d+)$/)[0]) } })
+
+album_pattern = /http:\/\/www\.xiami\.com\/album\/(\d+)/
+
+stub_request(:any, album_pattern).to_return(lambda { |request| { body: fixture('albums/' + request.uri.path.match(/(\d+)$/)[0]) } })
