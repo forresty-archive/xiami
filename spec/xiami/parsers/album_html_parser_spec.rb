e9c7ccbb543abc9441b9e17f30cd3221f552d58b
Forrest Ye <afu@forresty.com>
2015-03-25 20:11:22 +0800

    more spec

--- spec/xiami/parsers/album_html_parser_spec.rb
+++ spec/xiami/parsers/album_html_parser_spec.rb
@@ -3,10 +3,12 @@ require "spec_helper"
 module Xiami
   module Parser
     describe AlbumHTMLParser do
-      subject { AlbumHTMLParser.new(fixture('albums/56346', 'html')) }
+      it 'parses' do
+        expect(AlbumHTMLParser.new(fixture('albums/56346', 'html')).parse.size).to eq(12)
+      end
 
       it 'parses' do
-        expect(subject.parse.size).to eq(12)
+        expect(AlbumHTMLParser.new(fixture('albums/4424', 'html')).parse.size).to eq(11)
       end
     end
   end
