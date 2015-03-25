f0dd05284a0b4ff6a24c108527324249ea249915
Forrest Ye <afu@forresty.com>
2015-03-25 14:19:17 +0800

    transpec

--- spec/xiami/services/fetch_xiami_query_spec.rb
+++ spec/xiami/services/fetch_xiami_query_spec.rb
@@ -6,9 +6,8 @@ module Xiami
       context 'when query is "Dark Water & Stars"' do
         it 'encodes URL right' do
           service = FetchXiamiQuery.new(query: "Dark Water & Stars")
-          service.instance_eval do
-            url.should == 'http://www.xiami.com/search?key=Dark+Water+%26+Stars&pos=1'
-          end
+
+          expect(service.send(:url)).to eq('http://www.xiami.com/search?key=Dark+Water+%26+Stars&pos=1')
         end
       end
     end
--- spec/xiami/song_spec.rb
+++ spec/xiami/song_spec.rb
@@ -15,61 +15,61 @@ module Xiami
       it 'parses data' do
         song = Song.fetch('http://www.xiami.com/song/1773357685')
 
-        song.temporary_url.should == 'http://m5.file.xiami.com/516/37516/705574889/1773357685_15603838_l.mp3?auth_key=33b783883a2ffa9e9df48e5dd456ebad-1415232000-0-null'
+        expect(song.temporary_url).to eq('http://m5.file.xiami.com/516/37516/705574889/1773357685_15603838_l.mp3?auth_key=33b783883a2ffa9e9df48e5dd456ebad-1415232000-0-null')
 
-        song.name.should == 'Escape (The Pina Colada Song)'
-        song.title.should == 'Escape (The Pina Colada Song)'
-        song.artist_name.should == 'Rupert Holmes'
-        song.album_name.should == 'Guardians of the Galaxy'
+        expect(song.name).to eq('Escape (The Pina Colada Song)')
+        expect(song.title).to eq('Escape (The Pina Colada Song)')
+        expect(song.artist_name).to eq('Rupert Holmes')
+        expect(song.album_name).to eq('Guardians of the Galaxy')
 
-        song.album.id.should == 705574889
-        song.album.name.should == 'Guardians of the Galaxy'
-        song.album.cover_url.should == 'http://img.xiami.net/images/album/img16/37516/7055748891405574890_4.jpg'
+        expect(song.album.id).to eq(705574889)
+        expect(song.album.name).to eq('Guardians of the Galaxy')
+        expect(song.album.cover_url).to eq('http://img.xiami.net/images/album/img16/37516/7055748891405574890_4.jpg')
 
-        song.artist.id.should == 37516
-        song.artist.name.should == 'Rupert Holmes'
+        expect(song.artist.id).to eq(37516)
+        expect(song.artist.name).to eq('Rupert Holmes')
       end
 
       it 'accepts song id as well' do
         song = Song.fetch('1773357685')
 
-        song.album.id.should == 705574889
+        expect(song.album.id).to eq(705574889)
       end
 
       context 'when content contains html' do
         it 'unescapes' do
           song = Song.fetch(1242697)
-          song.album.name.should == "(What's the Story) Morning Glory?"
+          expect(song.album.name).to eq("(What's the Story) Morning Glory?")
         end
       end
 
       context 'when parsing xml fails' do
         it 'parses html' do
           song = Song.fetch(376054)
-          song.name.should == '她的睫毛'
-          song.id.should == 376054
-          song.album.id.should == 6650
-          song.album.name.should == '叶惠美'
-          song.album.cover_url.should == 'http://img.xiami.net/images/album/img60/1260/66501387132591_4.jpg'
-          song.artist.id.should == 1260
-          song.artist.name.should == '周杰伦'
+          expect(song.name).to eq('她的睫毛')
+          expect(song.id).to eq(376054)
+          expect(song.album.id).to eq(6650)
+          expect(song.album.name).to eq('叶惠美')
+          expect(song.album.cover_url).to eq('http://img.xiami.net/images/album/img60/1260/66501387132591_4.jpg')
+          expect(song.artist.id).to eq(1260)
+          expect(song.artist.name).to eq('周杰伦')
         end
 
         context 'with song id 1770769001' do
           it 'pass' do
             song = Song.fetch(1770769001)
-            song.artist.id.should == nil
-            song.artist.name.should == '梁博'
+            expect(song.artist.id).to eq(nil)
+            expect(song.artist.name).to eq('梁博')
           end
         end
 
         context 'with song id 15914' do
           it 'pass' do
             song = Song.fetch(15914)
-            song.artist.id.should == 521
-            song.artist.name.should == '李嘉强'
-            song.album.cover_urls.count.should == 4
-            song.album.cover_url.should == 'http://img.xiami.net/images/pic/04/04/10123658079m_2.jpg'
+            expect(song.artist.id).to eq(521)
+            expect(song.artist.name).to eq('李嘉强')
+            expect(song.album.cover_urls.count).to eq(4)
+            expect(song.album.cover_url).to eq('http://img.xiami.net/images/pic/04/04/10123658079m_2.jpg')
           end
         end
       end
@@ -82,10 +82,10 @@ module Xiami
       let(:song4) { Song.new.tap { |song| song.id = nil } }
 
       it 'test equality base on id' do
-        song1.should == song2
-        song2.should_not == song3
-        song1.should_not == nil
-        song1.should_not == song4
+        expect(song1).to eq(song2)
+        expect(song2).not_to eq(song3)
+        expect(song1).not_to eq(nil)
+        expect(song1).not_to eq(song4)
       end
     end
   end
